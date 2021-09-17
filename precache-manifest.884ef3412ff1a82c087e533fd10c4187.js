3c7966de338805e7bc9cd24b97b41bb1015c7ff1
edzyl <edzyl_gu@compal.com>
2021-09-17 17:38:58 +0800

    deploy

---
+++ precache-manifest.884ef3412ff1a82c087e533fd10c4187.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0cb00507d8030b0635",
+    "url": "/apple_bill/css/app.265099e5.css"
+  },
+  {
+    "revision": "6a5cb60e2b0b781e7b8d",
+    "url": "/apple_bill/css/chunk-vendors.7cff6ebe.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/apple_bill/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/apple_bill/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ebf6899f7c4a6b112ec41d052fb351ab",
+    "url": "/apple_bill/index.html"
+  },
+  {
+    "revision": "1b0cb00507d8030b0635",
+    "url": "/apple_bill/js/app.3cd29b33.js"
+  },
+  {
+    "revision": "6a5cb60e2b0b781e7b8d",
+    "url": "/apple_bill/js/chunk-vendors.d66dc176.js"
+  },
+  {
+    "revision": "5f3ceab733d98d87da35b188039e9159",
+    "url": "/apple_bill/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/apple_bill/robots.txt"
+  }
+]);
